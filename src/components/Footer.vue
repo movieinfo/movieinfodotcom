86709a9f8088c4ff4d3941409c9c86d79b345a97
maopos <mauricioposadamurillo@gmail.com>
2021-09-20 10:34:10 -0500

    Version 21.09.20

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <footer>
+            <p>
+                 Copyright © || MovieInfo 2021
+            </p>  
+        </footer>
+    </div>
+</template>
+
+<style scoped>
+
+    footer {
+    position: fixed;
+    height: 100px;
+    bottom: 0;
+    width: 100%;
+    background: #ccc;
+    display:flex;
+    
+
+    justify-content: center;
+    align-items: center;
+    }
+
+    p{
+        margin-top: 50px;
+    }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
-    
     <Header></Header>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 export default {
   name: 'Home',
   components: {
-      Header
+      Header,
+      Footer
   }
 }
 </script>
