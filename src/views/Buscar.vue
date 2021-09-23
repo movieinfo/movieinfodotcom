a58e37ce6acfd9924b6ece8ebf3c5e411f69f4e6
maopos <mauricioposadamurillo@gmail.com>
2021-09-23 18:48:23 -0500

    Version 21.09.23

---
+++ src/views/Buscar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Header></Header>
+    <h1>Componente Buscar</h1>
+   
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+export default {
+  components: {
+    Header,
+    Footer
+  },
+};
+</script>
