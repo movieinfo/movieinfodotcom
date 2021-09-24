8dd3d812a4b487c08d967bf88a96aa6d0cec16f8
maopos <mauricioposadamurillo@gmail.com>
2021-09-24 16:00:35 -0500

    Version 21.09.24

--- src/views/Buscar.vue
+++ src/views/Buscar.vue
@@ -1,7 +1,32 @@
 <template>
   <div>
     <Header></Header>
-    <h1>Componente Buscar</h1>
+    <div class="container">
+      <form v-on:submit.prevent="search">
+        
+        <br />
+        <br />
+        <div class="form-group">
+          <label>Busca tu pelicula favorita</label>
+          <br />
+          <br />
+
+          <input
+            type="text"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Nombre de la pelicula"
+            
+          />
+        </div>
+        <br />
+
+        <input type="submit" class="fadeIn fourth" value="Buscar" />
+      </form>
+      <br />
+      <br />
+    </div>
    
     <Footer></Footer>
   </div>
